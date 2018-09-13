a7a089a7b37a5b75643270f53568d210ec7a48e4
MarkPflug <MarkPflug@users.noreply.github.com>
2018-09-13 10:57:03 -0700

    sys info tool

---
+++ Elemental.SysInfoTool/Tool.cs
@@ -0,0 +1,163 @@
+﻿using Elemental;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+
+class Tool
+{
+    public static void Main(string[] args)
+    {
+        var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        var isOSX = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+        var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        if (isWindows)
+            EnableConsoleColor();
+
+        ColorConsole.SetForeground(0x40, 0x80, 0x20);
+        Value("Tool Version", typeof(Tool).Assembly.GetName().Version.ToString());
+        Header("Machine");
+        Value("Architecture", RuntimeInformation.ProcessArchitecture.ToString());
+        Value("Runtime Version", RuntimeEnvironment.GetSystemVersion());
+
+
+        Value("MachineName", Environment.MachineName);
+        var os =
+            isWindows
+            ? "Windows"
+            : isLinux
+            ? "Linux"
+            : isOSX
+            ? "OSX"
+            : "Other";
+        Value("OS", os);
+        Value("OSVersion", Environment.OSVersion.ToString());
+
+        Header("Time");
+        Value("UTC Time", DateTime.UtcNow.ToString());
+        Value("Local Time", DateTime.Now.ToString());
+        Value("TimeZone", TimeZoneInfo.Local.StandardName);
+
+        Header("Region/Culture");
+        Value("Region", RegionInfo.CurrentRegion.Name);
+        Value("Culture", CultureInfo.CurrentCulture.Name);
+        Value("UICulture", CultureInfo.CurrentUICulture.Name);
+
+        Header("User");
+        Value("Domain", Environment.UserDomainName);
+        Value("User", Environment.UserName);
+
+        Header("Network");
+
+        bool first = true;
+        foreach (var net in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            if (net.OperationalStatus == OperationalStatus.Up && net.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+            {
+                if (first) first = false;
+                else Console.WriteLine();
+                Value("Type", net.NetworkInterfaceType.ToString());
+                Value("Description", net.Description);
+                var props = net.GetIPProperties();
+                foreach (var addr in props.UnicastAddresses)
+                {
+                    Value(addr.Address.AddressFamily.ToString(), addr.Address.ToString());
+                }
+            }
+        }
+
+        Header("Environment");
+
+        foreach (var kvp in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>().OrderBy(kvp => kvp.Key))
+        {
+            var key = (string)kvp.Key;
+            var value = (string)kvp.Value;
+            if (SplitEnvVars.Contains(key))
+            {
+                var separator = isWindows ? ';' : ':';
+                var values = value.Split(separator);
+                Value(key, values, EnvVarWidth);
+            }
+            else
+            {
+                Value(key, value, EnvVarWidth);
+            }
+        }
+
+        ColorConsole.SetDefaults();
+    }
+
+    const int HeaderWidth = 80;
+    const int DefaultNameWidth = 16;
+    const int EnvVarWidth = 32;
+    const char HeaderChar = '-';
+    const int PrePadCount = 3;
+    static readonly string PrePad = new string(HeaderChar, PrePadCount);
+
+    static HashSet<string> SplitEnvVars = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { "Path", "PSModulePath", "PathEXT" };
+
+    static void EnableConsoleColor()
+    {
+        try
+        {
+            ColorConsole.EnableColorMode();
+        }
+        catch (Exception) {
+            // this might throw on linux/osx
+            // but we don't need to call
+        }
+    }
+
+    static void Header(string heading)
+    {
+        Console.WriteLine();
+        ColorConsole.SetForeground(0x40, 0xa0, 0xf0);
+
+        var l = HeaderWidth - 2 - PrePadCount - heading.Length;
+
+        Console.Write(PrePad);
+        Console.Write(' ');
+        Console.Write(heading);
+        Console.Write(' ');
+        if (l > 0)
+        {
+            Console.Write(new string(HeaderChar, l));
+        }
+        Console.WriteLine();
+        ColorConsole.SetDefaults();
+    }
+
+    static void Value(string name, string value, int width = DefaultNameWidth)
+    {
+        Value(name, new string[] { value }, width);
+    }
+
+    static void Value(string name, IEnumerable<string> values, int width = DefaultNameWidth)
+    {
+        ColorConsole.SetForeground(0x60, 0xc0, 0x60);
+        Console.Write(String.Format("{0," + width + "}", name));
+
+        ColorConsole.SetForeground(0xa0, 0xa0, 0xa0);
+        Console.Write(": ");
+
+        ColorConsole.SetForeground(0xe0, 0xe0, 0xe0);
+
+        bool first = true;
+        foreach(var value in values)
+        {
+            if (first) first = false;
+            else
+            {
+                Console.Write(new string(' ', width + 2));
+            }
+            Console.Write(value);
+            Console.WriteLine();
+        }
+        
+        ColorConsole.SetDefaults();
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Elemental SysInfo Tool
+
+A dotnet global tool that simply outputs system information about the current machine/environment.
+
+```
+# install
+dotnet tool install -g Elemental.SysInfoTool
+# run
+sysinfo
+```
+
+I've recently been playing around with Linux machines for the first time, and I always struggle to find basic information about the machine, like IP address and regional settings. I made this tool so I don't need to learn the OS-specific way to get at that information.
